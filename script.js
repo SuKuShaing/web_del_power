f64b463f5868a0b4df65bfe66fd12e63f8debece
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-06-13 20:55:24 -0400

    Se cambío el código de mucho if por un for en caso de que se desee ampliar el slice, solo se añade un input y añadirlo al array, no hay necesidad de expandir los if

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const r2 = document.getElementById('r2');
 const r3 = document.getElementById('r3');
 const r4 = document.getElementById('r4');
 
+// Para añadir más slice basta con agregar más inputs y agregarlos al array radios
+let radios = [r1, r2, r3, r4];
+
+
 // Crear un intervalo para cambiar el checked de los inputs
 const tiempoIntervalo = 4000;
 let fcIntervalo; // Guarda el número del intervalo y se utiliza para detenerlo
@@ -21,19 +25,31 @@ let IntervaloFuncionando = true; // Se implementó para evitar que se generen va
 function iniciarIntervalo() {
     fcIntervalo = setInterval(() => {
         IntervaloFuncionando = true;
-        if(r1.checked){
-            r2.checked = true;
-            r1.checked = false;
-        } else if(r2.checked){
-            r3.checked = true;
-            r2.checked = false;
-        } else if(r3.checked){
-            r4.checked = true;
-            r3.checked = false;
-        } else if(r4.checked){
-            r1.checked = true;
-            r4.checked = false;
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                radios[i].checked = false;
+                console.log(`radios[${i}].checked = false;`);
+                radios[(i + 1) % radios.length].checked = true;
+                console.log(`radios[${(i + 1) % radios.length}].checked = true;`);
+                break;
+            }
         }
+
+        // Primera versión, descartada porque si fuesen más se alargaría mucho
+        // if(r1.checked){
+        //     r2.checked = true;
+        //     r1.checked = false;
+        // } else if(r2.checked){
+        //     r3.checked = true;
+        //     r2.checked = false;
+        // } else if(r3.checked){
+        //     r4.checked = true;
+        //     r3.checked = false;
+        // } else if(r4.checked){
+        //     r1.checked = true;
+        //     r4.checked = false;
+        // }
+
     }, tiempoIntervalo);
     console.log("Intervalo N°: " + fcIntervalo);
 }
